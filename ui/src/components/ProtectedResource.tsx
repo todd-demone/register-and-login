ce24ad2db6082585303041940e93b88ae3da62af
Todd Demone <tdemone1@gmail.com>
2023-02-14 14:54:43 -0500

    created ProtectedResource component to display message retrieved from API using JWT

---
+++ ui/src/components/ProtectedResource.tsx
@@ -0,0 +1,42 @@
+import { Box, Container, CssBaseline, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { getProtectedResource } from "../services/user.service";
+
+function ProtectedResource() {
+  const [message, setMessage] = useState<string>("");
+
+  useEffect(() => {
+    getProtectedResource().then(
+      (response) => {
+        setMessage(response.data);
+      },
+      (error) => {
+        const _message =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString();
+
+        setMessage(_message);
+      }
+    );
+  }, []);
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h3">{message}</Typography>
+      </Box>
+    </Container>
+  );
+}
+
+export default ProtectedResource;
