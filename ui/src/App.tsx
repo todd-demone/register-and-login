c9a9cae7ee67b99196693397c2c1e401174e8838
Todd Demone <tdemone1@gmail.com>
2023-02-14 14:55:17 -0500

    add route for 'protected-resource'

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignUp from "./components/SignUp";
 import SignIn from "./components/SignIn";
+import ProtectedResource from "./components/ProtectedResource";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Route path="/" element={<SignUp />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
+        <Route path="/protected-resource" element={<ProtectedResource />} />
       </Routes>
     </Router>
   );
