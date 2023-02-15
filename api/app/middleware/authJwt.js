4732569cafba8871608196faca387564c6639aa7
Todd Demone <tdemone1@gmail.com>
2023-02-14 19:06:03 -0500

    Changed message received from API when non-logged in users attempt to view /protected-resource.

--- api/app/middleware/authJwt.js
+++ api/app/middleware/authJwt.js
@@ -5,7 +5,9 @@ verifyToken = (req, res, next) => {
   let token = req.headers["x-access-token"];
 
   if (!token) {
-    return res.status(403).send({ message: "No token provided!" });
+    return res
+      .status(403)
+      .send({ message: "You must sign in to view this page." });
   }
 
   jwt.verify(token, config.secret, (err, decoded) => {
